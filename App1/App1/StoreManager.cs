40bc5f2baf616042c7164282a265d590d2137dd6
moriyuki <noriyuki18@hotmail.com>
2016-01-09 13:47:59 +0900

    StoreManagerClass xmlファイル新規作成関数作成

--- App1/App1/StoreManager.cs
+++ App1/App1/StoreManager.cs
@@ -11,6 +11,31 @@ namespace App1
     // ローカルデータ管理クラス
     class StoreManager
     {
+        // ローカルデータファイルが無いときに新規作成する
+        void createDataFile()
+        {
+            XmlDocument datafile = new XmlDocument();
+
+            XmlDeclaration declaration = datafile.CreateXmlDeclaration("1.0", "Shift-JIS", null);
+            XmlElement root = datafile.CreateElement("root");
+
+            datafile.AppendChild(declaration);
+            datafile.AppendChild(root);
+
+            XmlElement element = datafile.CreateElement("element");
+
+            element.InnerText = "text";
+            element.SetAttribute("attribute", "256");
+
+            root.AppendChild(element);
+
+            using (StreamWriter writer = File.CreateText("sample.xml"))
+            {
+                datafile.Save(writer);
+            }
+
+        }
+
         // データを呼び出すメソッド（XMLファイルから）
         // データを書き込むメソッド（XMLファイルに）
     }
