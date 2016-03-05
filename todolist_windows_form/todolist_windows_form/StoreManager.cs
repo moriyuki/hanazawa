e3431e6885cdc064df84d3efb105d1f3d4fe258d
alexfujimura <y.fujimura@do-will.org>
2016-03-05 16:10:10 +0900

    StoreManagerのSaveXML

--- todolist_windows_form/todolist_windows_form/StoreManager.cs
+++ todolist_windows_form/todolist_windows_form/StoreManager.cs
@@ -30,7 +30,7 @@ namespace todolist_windows_form
             element.SetAttribute("attribute", "256");
 
             root.AppendChild(element);
-            
+
             datafile.Save("test.xml");
         }
 
@@ -47,11 +47,11 @@ namespace todolist_windows_form
                 {
                     // Console.WriteLine(" find node!: {0} ", reader.NodeType.ToString());
 
-                    if ( reader.NodeType.Equals(XmlNodeType.Element) && reader.Name.Equals("issue") )
+                    if (reader.NodeType.Equals(XmlNodeType.Element) && reader.Name.Equals("issue"))
                     {
                         DataModel.ticket ticket = new DataModel.ticket();
                         ticket.initTicket();
-                        
+
 
                         while (reader.Read())
                         {
@@ -156,7 +156,7 @@ namespace todolist_windows_form
                             else if (reader.NodeType.Equals(XmlNodeType.Element) && reader.Name.Equals("due_date"))
                             {
                                 // due_date 読み込み
-                                    ticket.due_date = ReadDateTimeFromReeader(reader);
+                                ticket.due_date = ReadDateTimeFromReeader(reader);
                             }
                             else if (reader.NodeType.Equals(XmlNodeType.Element) && reader.Name.Equals("done_ratio"))
                             {
@@ -212,7 +212,7 @@ namespace todolist_windows_form
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 System.Windows.Forms.MessageBox.Show(ex.ToString());
             }
@@ -233,6 +233,94 @@ namespace todolist_windows_form
             DataModel dm = DataModel.GetInstance();
 
             // xml dataとして保存
+            XmlDocument datafile = new XmlDocument();
+
+            XmlDeclaration declaration = datafile.CreateXmlDeclaration("1.0", "Shift-JIS", null);
+            XmlElement issues = datafile.CreateElement("issues");
+
+            datafile.AppendChild(declaration);
+            datafile.AppendChild(issues);
+
+            for (int i = 0; i < dm.tickets.Count; i++)
+            {
+
+                XmlElement issue = datafile.CreateElement("issue");
+
+                XmlElement id = datafile.CreateElement("id");
+                id.InnerText = dm.tickets[i].id.ToString();
+                issue.AppendChild(id);
+
+                XmlElement project = datafile.CreateElement("project");
+                project.SetAttribute("name", dm.tickets[i].project.name);
+                project.SetAttribute("id", dm.tickets[i].project.id.ToString());
+                issue.AppendChild(project);
+
+                XmlElement tracker = datafile.CreateElement("tracker");
+                tracker.SetAttribute("name", dm.tickets[i].tracker.name);
+                tracker.SetAttribute("id", dm.tickets[i].tracker.id.ToString());
+                issue.AppendChild(tracker);
+
+                XmlElement status = datafile.CreateElement("status");
+                status.SetAttribute("name", dm.tickets[i].status.name);
+                status.SetAttribute("id", dm.tickets[i].status.id.ToString());
+                issue.AppendChild(tracker);
+
+                XmlElement priority = datafile.CreateElement("priority");
+                priority.SetAttribute("name", dm.tickets[i].priority.name);
+                priority.SetAttribute("id", dm.tickets[i].priority.id.ToString());
+                issue.AppendChild(priority);
+
+                XmlElement author = datafile.CreateElement("author");
+                author.SetAttribute("name", dm.tickets[i].auther.name);
+                author.SetAttribute("id", dm.tickets[i].auther.id.ToString());
+                issue.AppendChild(author);
+
+                XmlElement subject = datafile.CreateElement("subject");
+                subject.InnerText = dm.tickets[i].subject;
+                issue.AppendChild(subject);
+
+                XmlElement description = datafile.CreateElement("description");
+                issue.AppendChild(description);
+
+                XmlElement start_date = datafile.CreateElement("start_date");
+                start_date.InnerText = dm.tickets[i].start_date.ToString();
+                issue.AppendChild(start_date);
+
+                XmlElement due_date = datafile.CreateElement("due_date");
+                due_date.InnerText = dm.tickets[i].due_date.ToString();
+                issue.AppendChild(due_date);
+
+                XmlElement done_ratio = datafile.CreateElement("done_ratio");
+                done_ratio.InnerText = dm.tickets[i].done_ratio.ToString();
+                issue.AppendChild(done_ratio);
+
+                XmlElement is_private = datafile.CreateElement("is_private");
+                is_private.InnerText = dm.tickets[i].is_private.ToString();
+                issue.AppendChild(is_private);
+
+                XmlElement estimated_hours = datafile.CreateElement("estimated_hours");
+                estimated_hours.InnerText = dm.tickets[i].estimated_hours.ToString();
+                issue.AppendChild(estimated_hours);
+
+                XmlElement created_on = datafile.CreateElement("created_on");
+                created_on.InnerText = dm.tickets[i].created_on.ToString();
+                issue.AppendChild(created_on);
+
+                XmlElement updated_on = datafile.CreateElement("updated_on");
+                updated_on.InnerText = dm.tickets[i].updated_on.ToString();
+                issue.AppendChild(updated_on);
+
+                XmlElement closed_on = datafile.CreateElement("closed_on");
+                closed_on.InnerText = dm.tickets[i].closed_on.ToString();
+                issue.AppendChild(closed_on);
+
+
+                issues.AppendChild(issue);
+            }
+
+
+
+            datafile.Save("test.xml");
         }
     }
 }
